b4497a52004ac1b451163cd4bbb1b266bbd00c22
Phillip Scanlon <fatmailbox@gmail.com>
2017-12-07 14:34:57 +0800

    Correct spinner file added

--- Spinner_3Neurons.ino
+++ Spinner_3Neurons.ino
@@ -1,20 +1,23 @@
 /*
-Dekatron Spinner - Variable Rate....
+Dekatron Pendulum Example
 
 modified March 25, 2017
 by Michael Moorrees
 */
 
 int D_count = 0;
-int i_rate = 15;
-int i_delay = 100;
-int LED = 13;         // Test LED
+int i_count = 0;
+int state = 0;
+int LED = 0;          // Test LED
 int Guide1 = 5;       // Guide 1 - G1 pin of 2-guide Dekatron
 int Guide2 = 6;       // Guide 2 - G2 pin of 2-guide Dekatron
-int Index = 7;       // Index   - NDX input pin. High when glow at K0
-int Ndx = 0;          // K0 index indicator variable.
+int Index = 7;        // Index   - NDX input pin. High when glow at K0
+boolean Dir = false;  // Direction Flag, true for clockwise
+int Ndx = 0;          // K0 index indicator variable
+long DelayRamp = 0;
 
-void G_step(int CINT) // Dekatron Stepper
+// Dekatron Stepper
+void G_step(int CINT)
 {
 	if (CINT > 2) CINT = 0;
 	if (CINT == 0)
@@ -42,27 +45,47 @@ void setup() {
 	pinMode(LED, OUTPUT);
 
 	D_count = 0;
-	i_rate = 15;
-	i_delay = 100;
+	i_count = 0;
+	state = 0;
 	Ndx = 0;
+	Dir = true;
 	G_step(D_count);
 	digitalWrite(LED, LOW);
+	DelayRamp = 0;
 }
 
 // the loop function runs over and over again forever
 void loop() {
-	//	Ndx = digitalRead(Index); // Sample for glow at K0
-	//	if (Ndx) {
-	//		i_rate = 16;
-	//		i_delay = (3 * i_rate) + 8;
-	//		digitalWrite(LED, HIGH);
-	//	}
-	i_rate--;
-	i_delay = (3 * abs(i_rate)) + 8;
-	D_count++;
-	delay(i_delay);            // wait per i_rate
+	Ndx = digitalRead(Index);			// Sample for glow at K0
+	if (Ndx) digitalWrite(LED, HIGH);
+	if (!Ndx) digitalWrite(LED, LOW);
+	if (Ndx && state == 0)				// If yes, and state = 0,
+	{
+		state = 1;						// then, set to state 1 to ignore Ndx for 5mS        
+		i_count = 0;					// clr ignore counter (i_count)       
+										//Dir = !Dir;					// flip direction (Dir)
+
+	}
+	if (Dir)
+	{
+		D_count++;						// increment D_count for clockwise motion
+		i_count++;						// increment ignore period counter
+	}
+	else
+	{
+		D_count--;						// decrement D_count for counter-clockwise motion
+		i_count++;						// increment ignore period counter
+	}
+	if (i_count == 5)					// at count of 5, stop ignoring Ndx, to allow looking for K0 glow.
+	{
+		state = 0;
+		//digitalWrite(LED, LOW);  
+	}
+	delay(DelayRamp);
+	if (Ndx)DelayRamp = DelayRamp + 1000;
+	if (DelayRamp > 10000) DelayRamp = 1;
+	Serial.println(DelayRamp);
 	if (D_count > 2) D_count = 0;
 	if (D_count < 0) D_count = 2;
-	G_step(D_count);          // Step Dekatron
-	digitalWrite(LED, LOW);
+	G_step(D_count);					// Step Dekatron
 }
